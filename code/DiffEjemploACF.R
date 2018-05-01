4d4fc563a8fb89b94a3d24081018cd67004b4771
ameilij <ariel.meilij@gmail.com>
2018-05-01 17:34:50 -0500

    Primer borrador Marco Teorico Series de Tiempo.

--- code/DiffEjemploACF.R
+++ code/DiffEjemploACF.R
@@ -1,10 +1,14 @@
 # UBJ - Doctoral Thesis
 # Marco Teorico - Series de Tiempo
-# Ejemplo de plot ACF de una serie de tiempo
+# Ejemplo de plot ACF de series de tiempo y series diferenciadas
 
 library(Quandl)
 library(forecast)
 Quandl.api_key("KzzS8Vfxkw1ZgTWgU4jH")
 COP_raw <- Quandl("CURRFX/USDCOP", collapse = "monthly", type = "ts")
 COP <- COP_raw[,1]
-Acf(COP, main = "Grafica ACF Peso Colombiano")
+COP_diff <- diff(COP)
+
+par(mfrow=c(1,2))
+Acf(COP, main = "ACF Peso Colombiano")
+Acf(COP_diff, main = "ACF Peso Colombiano Diferenciado")
